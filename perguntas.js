fd6c914d6d284fdafcbfb0c180215ee3352ea671
xandydrv <149505974+xandydrv@users.noreply.github.com>
2024-09-22 14:28:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Filosofia',
+    'Onde surgiu os primeiros filósofos ?',
+    'Surgiu na Grecia Antiga'
+)
+
+criaCartao(
+    'Geopolitica',
+    'oque é a geopolitica?',
+    'uma área que se concentra na interseção entre geografia, relações internacionais e dinâmica de poder global.'
+)
+
+criaCartao(
+   'fisica ',
+    'Qual a formula da variação da velocidade ?',
+    'v = Δs/Δt, onde Δs é o deslocamento e Δt é o intervalo de tempo.  '
+)
+
+criaCartao(
+'biologia',
+'qual a particula fundamental da materia?',
+'Os Atomos'
+)
+
+criaCartao(
+    'historia ',
+    'em qual ano acabou a 2 Guerra Mundial?',
+    'em 1945'
+)
+
+
+criaCartao(
+    'portugues ',
+    'oque é uma silaba?',
+    'Sílabas são as unidades sonoras de uma palavra, formadas por vogais e, às vezes, por consoantes também'
+)
